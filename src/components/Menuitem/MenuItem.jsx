95858b009129aa3d7daf6a0ccd492cfe5d880005
Furkan <mfurkan.firuz@gmail.com>
2022-03-28 01:28:45 +0300

    Special menu created and responsive fixed

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      <div className="app__menuitem-name">
+        <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      </div>
+      <div className="app__menuitem-dash" />
+      <div className="app__menuitem-price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+
+    <div className="app__menuitem-sub">
+      <p className="p__opensans" style={{ color: '#AAAAAA' }}>{tags}</p>
+    </div>
   </div>
 );
 
